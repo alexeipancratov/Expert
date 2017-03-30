599dccbadc8248b7ad3fb54acb8a2e928e9b0275
pplesca <pplesca@EN32177>
2017-03-30 11:04:00 +0300

    added Autofac IoC

--- Expert.WebApi/Global.asax.cs
+++ Expert.WebApi/Global.asax.cs
@@ -3,7 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
+using System.Web.Mvc;
 using System.Web.Routing;
+using Autofac.Integration.Mvc;
+using Autofac.Integration.WebApi;
+using Expert.WebApi.Infrastructure;
 
 namespace Expert.WebApi
 {
@@ -12,6 +16,11 @@ namespace Expert.WebApi
         protected void Application_Start()
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
+
+            var container = DependencyBuilder.Build();
+
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+            GlobalConfiguration.Configuration.DependencyResolver = new AutofacWebApiDependencyResolver(container);
         }
     }
 }
---
+++ Expert.WebApi/Infrastructure/DependencyBuilder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Autofac;
+
+namespace Expert.WebApi.Infrastructure
+{
+    public class DependencyBuilder
+    {
+        public static IContainer Build()
+        {
+            var builder = new ContainerBuilder();
+
+            return builder.Build();
+        }
+    }
+}
