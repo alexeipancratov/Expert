6e2e4f31ca1021cc24431044f48c35b22f6158e8
pplesca <pplesca@EN32177>
2017-03-31 17:55:40 +0300

    Added CORS

--- Expert.WebApi/Global.asax.cs
+++ Expert.WebApi/Global.asax.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
+using System.Web.Http.Cors;
 using System.Web.Mvc;
 using System.Web.Routing;
 using Autofac.Integration.Mvc;
@@ -16,6 +17,10 @@ namespace Expert.WebApi
         protected void Application_Start()
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
+
+            var corsAttr = new EnableCorsAttribute("http://<UI_Address>", "*", "*");
+            corsAttr.SupportsCredentials = true;
+            GlobalConfiguration.Configuration.EnableCors(corsAttr);
 
             var container = DependencyBuilder.Build();
 
