834dc839adbeaa393aee85285682fcdbb437cfd6
pplesca <pplesca@EN32177>
2017-03-31 17:28:13 +0300

    Created the domain model and enabled Windows Authetication on the backend

---
+++ Expert.WebApi/Controllers/CategoriesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Expert.WebApi.Controllers
+{
+    [RoutePrefix("categories")]
+    public class CategoriesController : ApiController
+    {
+        [HttpGet]
+        [Route]
+        public IHttpActionResult GetCategories()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
