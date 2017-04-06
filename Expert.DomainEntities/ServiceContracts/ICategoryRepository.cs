2d78544aff63deca97352b0c4ae44277a8394eac
pplesca <pplesca@EN32177>
2017-04-06 17:05:51 +0300

    added the category repository

---
+++ Expert.DomainEntities/ServiceContracts/ICategoryRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Expert.DomainEntities.Entities;
+
+namespace Expert.DomainEntities.ServiceContracts
+{
+    public interface ICategoryRepository
+    {
+        List<Category> GetCategories();
+
+        IQueryable<Category> GetCategoryByFilter(Expression<Func<Category, bool>> expression);
+    }
+}
