9fe6c39cba462f70836d52aaee1dc0d3ed028116
Alexei Pancratov <apancratov@endava.net>
2017-04-11 18:09:03 +0300

    Added properties for the Answer entity class

--- Expert.DomainEntities/Entities/Answer.cs
+++ Expert.DomainEntities/Entities/Answer.cs
@@ -1,6 +1,19 @@
-﻿namespace Expert.DomainEntities.Entities
+﻿using System;
+
+namespace Expert.DomainEntities.Entities
 {
     public class Answer : CoreEntity
     {
+        public Question Question { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        public User User { get; set; }
+
+        public string Content { get; set; }
+
+        public bool Accepted { get; set; }
+
+        public int Likes { get; set; }
     }
 }
--- Expert.DomainEntities/Entities/Category.cs
+++ Expert.DomainEntities/Entities/Category.cs
@@ -2,7 +2,8 @@
 {
     public class Category : CoreEntity
     {
-        public string Name { get; set; }    
+        public string Name { get; set; }
+
         public string Description { get; set; }
     }
 }
