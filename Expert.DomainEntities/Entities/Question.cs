46d4999edc49afbacbffe3fcc3ca09ca9d638dad
pplesca <pplesca@EN32177>
2017-04-11 18:10:07 +0300

    Filled the fields for Question and User

--- Expert.DomainEntities/Entities/Question.cs
+++ Expert.DomainEntities/Entities/Question.cs
@@ -1,6 +1,17 @@
-﻿namespace Expert.DomainEntities.Entities
+﻿using System;
+
+namespace Expert.DomainEntities.Entities
 {
     public class Question : CoreEntity
     {
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public string UserId { get; set; }
+        public string CategoryId { get; set; }
+        public string TagId { get; set; }
+        public int Likes { get; set; }
     }
 }
--- Expert.DomainEntities/Entities/User.cs
+++ Expert.DomainEntities/Entities/User.cs
@@ -1,6 +1,16 @@
-﻿namespace Expert.DomainEntities.Entities
+﻿using System.Collections.Generic;
+
+namespace Expert.DomainEntities.Entities
 {
     public class User : CoreEntity
     {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+        public int Level { get; set; }
+
+        public string Department { get; set; }
+
+        public List<int> Categories { get; set; }   
     }
 }
